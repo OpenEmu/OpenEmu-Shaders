b495b04c8316edf58b818e7e339b90718c303796
J-rg <j_rg@live.de>
2021-10-23 20:54:11 +0200

    chore: This is v1.19.2

--- Package.swift
+++ Package.swift
@@ -1,27 +1,27 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "1.19.1"
-let tag = "v1.19.1"
-let checksum = "32bbd8eb7516e37f6cb643ecd6e19a8380b9cc2f04b898eb7ab37c915ff05276"
+let version = "1.19.2"
+let tag = "v1.19.2"
+let checksum = "335715c1664f8c2ada476c65a8b35715ee29e36ee19ec3388bd517750fd1fe4f"
 let url = "https://github.com/OpenEmu/OpenEmu-Shaders/releases/download/\(tag)/OpenEmuShaders-\(version).xcframework.zip"
 
 let package = Package(
     name: "OpenEmuShaders",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_14),
     ],
     products: [
         .library(
             name: "OpenEmuShaders",
             targets: ["OpenEmuShaders"]
-        )
+        ),
     ],
     targets: [
         .binaryTarget(
             name: "OpenEmuShaders",
             url: url,
             checksum: checksum
-        )
+        ),
     ]
 )
